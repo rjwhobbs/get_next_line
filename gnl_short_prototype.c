b6f6430d58cc8439cafd228e9749806f27a19884
Roger HOBBS <rhobbs@c4r15s7.wethinkcode.co.za>
2019-07-01 14:13:54 +0200

    shortened

--- gnl_short_prototype.c
+++ gnl_short_prototype.c
@@ -1,5 +1,4 @@
 #include "get_next_line.h"
-#include "libft.h"
 
 char	*ft_strrealloc(char *s1, char *s2)
 {
@@ -10,7 +9,7 @@ char	*ft_strrealloc(char *s1, char *s2)
 	return (new);
 }
 
-static int		temp_check(char **temp, char **line, char *del_temp)
+static int		temp_check(char **temp, char **line, char **del_temp)
 {
 	char *nlp;
 
@@ -26,15 +25,15 @@ static int		temp_check(char **temp, char **line, char *del_temp)
 			return (1);
 		}
 		*line = ft_strrealloc(*line, *temp);
-		ft_strdel(&del_temp);
+		ft_strdel(del_temp);
 	}
 	return (0);
 }
 
-void		buff_rw(char **temp, char **line, char *del_temp, int *end, char **nlp, int fd)
+void		buff_rw(char **temp, char **line, char **del_temp, int *end, char **nlp, int fd)
 {
 	char 		buff[BUFF_SIZE + 1];
-
+	
 	*end = read(fd, buff, BUFF_SIZE);
 	buff[*end] = '\0';
 	*nlp = ft_strchr(buff, '\n');
@@ -42,7 +41,7 @@ void		buff_rw(char **temp, char **line, char *del_temp, int *end, char **nlp, in
 	{
 		**nlp = '\0';
 		*temp = ft_strdup(*nlp + 1);
-		del_temp = *temp;
+		*del_temp = *temp;
 	}
 	*line = ft_strrealloc(*line, buff);
 }
@@ -57,15 +56,15 @@ int	get_next_line(const int fd, char **line)
 	static char		*del_temp;
 
 	*line = ft_strnew(0);
-	if (temp_check(&temp, line, del_temp))
+	if (temp_check(&temp, line, &del_temp))
 		return (1);
 	if (last_line == 1)
 		return (0);
 	end = BUFF_SIZE;
 	while (end == BUFF_SIZE)
 	{
-		buff_rw(&temp, line, del_temp, &end, &nlp, fd);
-		if (*nlp)
+		buff_rw(&temp, line, &del_temp, &end, &nlp, fd);
+		if (nlp)
 			return (1);
 	/*	end = read(fd, buff, BUFF_SIZE);
 		buff[end] = '\0';
